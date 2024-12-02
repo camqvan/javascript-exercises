55546b8afcefbdca2e280b3dba6205f38a8565fc
Cam Van <camqvan@gmail.comls>
2024-12-01 18:14:47 -0800

    complete exercise 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+// newArray = [1, 2, 3, 4];
+
+const removeFromArray = function (newArray, num) {
+  spreadArray = [...newArray];
+  for (i = 0; i < spreadArray.length; i++) {
+    if (spreadArray[i] == num) {
+      spreadArray.splice(i, 1); // At the current index, remove one element
+    }
+  }
+  return spreadArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  let arr = [min, max];
+  let sum = 0;
 
+  for (let i = Math.min.apply(null, arr); i <= Math.max.apply(null, arr); i++) {
+    sum += i;
+  }
+  return sum;
 };
+
+// console.log(sumAll([1, 4]));
 
 // Do not edit below this line
 module.exports = sumAll;
