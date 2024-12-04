4b557054e18f3faee2f9ab89dfc0073b7ea46ab1
Cam Van <camqvan@gmail.comls>
2024-12-03 20:19:13 -0800

    8-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
+  return people.reduce((old, older) => {
+    const oldAge = getAge(old.yearOfBirth, old.yearOfDeath);
+    const olderAge = getAge(older.yearOfBirth, older.yearOfDeath);
+
+    return oldAge < olderAge ? older : old;
+  });
 };
 
 // Do not edit below this line
